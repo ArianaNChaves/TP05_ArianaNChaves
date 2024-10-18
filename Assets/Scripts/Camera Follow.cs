39b833c82b3ac6780e56ad35bd4e9888a6a33efb
ArianaNChaves <navi.ariana99@gmail.com>
2024-10-18 13:36:02 -0300

    Assets

---
+++ Assets/Scripts/Camera Follow.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private float speed = 2.0f;
+    [SerializeField] private float yOffset = 1.0f;
+    private void LateUpdate()
+    {
+        Vector3 position = new Vector3(target.position.x, target.position.y + yOffset, transform.position.z);
+        transform.position = Vector3.Slerp(transform.position, position, speed * Time.deltaTime);
+    }
+}
