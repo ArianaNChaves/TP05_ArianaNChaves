eb32aac40fc63278a5a0e677dbec73da16985259
ArianaNChaves <navi.ariana99@gmail.com>
2024-10-21 15:56:11 -0300

    fix Camera bug

--- Assets/Scripts/Camera Follow.cs
+++ Assets/Scripts/Camera Follow.cs
@@ -9,6 +9,12 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] private float yOffset = 1.0f;
     private void LateUpdate()
     {
+        FollowTarget();
+    }
+
+    private void FollowTarget()
+    {
+        if (target == null) return;
         Vector3 position = new Vector3(target.position.x, target.position.y + yOffset, transform.position.z);
         transform.position = Vector3.Slerp(transform.position, position, speed * Time.deltaTime);
     }
